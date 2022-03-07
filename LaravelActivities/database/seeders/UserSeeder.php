feabf7c15e63bb41fcfc627e120e6910019f15b3
FajardoMargarette <78639600+FajardoMargarette@users.noreply.github.com>
2022-03-07 18:39:46 +0800

    Acitivity 3

---
+++ LaravelActivities/database/seeders/UserSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Str;
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('users')->insert([
+            'name' =>Str::random(15),
+            'email' => Str::random(15).'@gmail.com',
+            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+
+        ]);
+    }
+}
